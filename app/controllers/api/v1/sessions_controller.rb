af237a2e2ec8192ea7cc1ca1874f11c4e5a70409
Torvick <vh.urquides@gmail.com>
2018-10-14 01:10:14 -0500

    Remove Validate CheckIn

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,3 @@
 class Api::V1::SessionsController < Api::V1::BaseController
   before_action :doorkeeper_authorize!
-  # before_action :authenticate_with_token!, only: :destroy
 end
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,14 +2,14 @@ class Registration < ApplicationRecord
   belongs_to :user
   # before_create :validate_day?
 
-  with_options if: Proc.new { |t| t.user.present? } do |with_users|
-    with_users.validate :validate_day?
-  end
-
-  def validate_day?
-    # self.check_in = Time.now
-    puts Registration.where(user: self.user).where(check_in: self.check_in).inspect
-    errors.add(:check_in, "can't be same as the receiving user.") if !Registration.where(user: self.user).where(check_in: self.check_in).empty?
-    raise "#{!Registration.where(user: self.user).where(check_in: self.check_in).empty?}-#{Registration.where(user: self.user).where(check_in: self.check_in).empty?}"
-  end
+  # with_options if: Proc.new { |t| t.user.present? } do |with_users|
+  #   with_users.validate :validate_day?
+  # end
+  #
+  # def validate_day?
+  #   # self.check_in = Time.now
+  #   puts Registration.where(user: self.user).where(check_in: self.check_in).inspect
+  #   errors.add(:check_in, "can't be same as the receiving user.") if !Registration.where(user: self.user).where(check_in: self.check_in).empty?
+  #   raise "#{!Registration.where(user: self.user).where(check_in: self.check_in).empty?}-#{Registration.where(user: self.user).where(check_in: self.check_in).empty?}"
+  # end
 end
